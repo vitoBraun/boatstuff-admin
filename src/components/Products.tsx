a235be94c7e8d98c6de963e1feae89d39aea83b2
Vitalii Braun <vitossvx@gmail.com>
2023-09-19 00:14:39 +0300

    first commit

---
+++ src/components/Products.tsx
@@ -0,0 +1,113 @@
+import React, { ReactElement } from "react";
+import { useQuery } from "react-query";
+
+type ICategory = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+type Product = {
+  id: string;
+  title: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  isNew: boolean;
+  isAvailable: boolean;
+  categories: ICategory[];
+  images?: string[];
+};
+
+const fetchProducts = async () => {
+  const response = await fetch("http://localhost:1333/product/list");
+  return response.json();
+};
+
+export default function Products() {
+  const {
+    data: products,
+    isLoading,
+    isError,
+  } = useQuery<Product[]>("products", fetchProducts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading products</div>;
+  }
+
+  const Headers = [
+    "id",
+    "title",
+    "short description",
+    "availability",
+    "categories",
+    "price",
+    "images",
+  ];
+
+  const Cell = ({
+    text,
+  }: {
+    text?: string | number | ReactElement | string[];
+  }): ReactElement => {
+    return (
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {text}
+      </td>
+    );
+  };
+  return (
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full">
+              <thead className="bg-white border-b">
+                <tr>
+                  {Headers.map((header) => (
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      {header}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {products?.map((product) => (
+                  <tr
+                    className="bg-gray-100 border-b hover:bg-gray-200 cursor-pointer"
+                    onClick={() => {
+                      alert(product.id);
+                    }}
+                  >
+                    <Cell text={product.id} />
+                    <Cell text={product.title} />
+                    <Cell text={product.shortDescription} />
+                    <Cell text={product.isAvailable ? "YES" : "NO"} />
+                    <Cell
+                      text={
+                        <ul>
+                          {product.categories.map((category) => (
+                            <li>{category.title}</li>
+                          ))}
+                        </ul>
+                      }
+                    />
+                    <Cell text={product.price} />
+                    <Cell text={product.images} />
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
